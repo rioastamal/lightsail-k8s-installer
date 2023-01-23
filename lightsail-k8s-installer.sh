2439845721973b43e73258b8e26cd50c2c41fccb
Rio Astamal <me@rioastamal.net>
2023-01-23 16:55:26 +0000

    Add command line options and code improvements

--- lightsail-k8s-installer.sh
+++ lightsail-k8s-installer.sh
@@ -3,9 +3,10 @@
 # @author Rio Astamal <rio@rioastamal.net>
 # @desc Script to automate Kubernetes installation on Amazon Lightsail instances
 
+readonly LK8S_SCRIPT_NAME=$( basename "$0" )
 LK8S_BASEDIR=$( realpath "$( dirname "$0" )" )
 LK8S_VERSION="2023-01-21"
-LK8S_CONFIG_FILE=""
+LK8S_DEBUG="true"
 
 # Path to directory to store application outputs
 [ ! -d "$LK8S_OUTPUT_DIR/.out" ] && LK8S_OUTPUT_DIR="$LK8S_BASEDIR/.out"
@@ -31,11 +32,7 @@ LK8S_NUMBER_OF_CP_NODES=1
 # Only amazon_linux_2 is supported at the moment.
 LK8S_CP_OS_ID="amazon_linux_2"
 LK8S_WORKER_OS_ID="amazon_linux_2"
-LK8S_CLOUDFORMATION_STACK_ID=$1
-LK8S_CLOUDFORMATION_STACKNAME=$LK8S_CLOUDFORMATION_STACKNAME_PREFIX-$LK8S_CLOUDFORMATION_STACK_ID
-LK8S_CONTROL_PLANE_NODE_PREFIX=$LK8S_CONTROL_PLANE_NODE_PREFIX-$LK8S_CLOUDFORMATION_STACKNAME
-LK8S_WORKER_NODE_PREFIX=$LK8S_WORKER_NODE_PREFIX-$LK8S_CLOUDFORMATION_STACKNAME
-LK8S_WORKER_LOAD_BALANCER_PREFIX=$LK8S_WORKER_LOAD_BALANCER_PREFIX-$LK8S_CLOUDFORMATION_STACKNAME
+LK8S_INSTALLATION_ID=$1
 
 # See all available Bundle ID using CLI: `aws lightsail get-bundles`
 # micro_2_0 -> 1vCPU & 1GB RAM
@@ -86,17 +83,71 @@ EOF
 EOF
 )
 
+# Function to show the help message
+lk8s_help()
+{
+    echo "\
+Usage: $0 [OPTIONS]
+
+Where OPTIONS:
+  -c CIDR       pod network specified by CIDR
+  -d ID         destroy installation specified by ID
+  -h            print this help and exit
+  -i ID         specify installation using ID
+  -r            dry run mode, print CloudFormation template and exit
+  -w NUM        number of worker nodes specified by NUM
+  -v            print script version
+
+lightsail-k8s-installer is a command line interface to bootstrap Kubernetes 
+cluster on Amazon Lightsail. lightsail-k8s-installer is free software licensed 
+under MIT. Visit the project homepage at 
+http://github.com/rioastamal/lightsail-k8s-installer."
+}
+
+lk8s_write_log()
+{
+    _LOG_MESSAGE="$@"
+    _SYSLOG_DATE_STYLE=$( date +"%b %e %H:%M:%S" )
+
+    # Date Hostname AppName[PID]: MESSAGE
+    printf "[%s LK8S]: %s\n" \
+        "$_SYSLOG_DATE_STYLE" \
+        "${_LOG_MESSAGE}">> "$LK8S_LOG_FILE"
+}
+
+lk8s_log()
+{
+    [ "$LK8S_DEBUG" = "true" ] && echo "[LK8S]: $@"
+    lk8s_write_log "$@"
+}
+
+lk8s_log_waiting()
+{
+    [ "$LK8S_DEBUG" = "true" ] && echo -en "\r[LK8S]: $@\033[K"
+    lk8s_write_log "$@"
+}
+
+lk8s_err() {
+    echo "[ERROR]: $@" >&2
+    lk8s_write_log "$@"
+}
+
 lk8s_init()
 {
   mkdir -p "$LK8S_OUTPUT_DIR"
   
-  # Load config file if specified
-  [ -f "$LK8S_CONFIG_FILE" ] && source "$LK8S_CONFIG_FILE"
-  
-  [ -z "$LK8S_CLOUDFORMATION_STACK_ID" ] && {
-    echo "Missing stack id." >&1
+  [ -z "$LK8S_INSTALLATION_ID" ] && {
+    echo "Missing installation id. See -h for help." >&2
     return 1
   }
+  
+  LK8S_CLOUDFORMATION_STACKNAME=$LK8S_CLOUDFORMATION_STACKNAME_PREFIX-$LK8S_INSTALLATION_ID
+  LK8S_CONTROL_PLANE_NODE_PREFIX=$LK8S_CONTROL_PLANE_NODE_PREFIX-$LK8S_CLOUDFORMATION_STACKNAME
+  LK8S_WORKER_NODE_PREFIX=$LK8S_WORKER_NODE_PREFIX-$LK8S_CLOUDFORMATION_STACKNAME
+  LK8S_WORKER_LOAD_BALANCER_PREFIX=$LK8S_WORKER_LOAD_BALANCER_PREFIX-$LK8S_CLOUDFORMATION_STACKNAME
+  
+  local _LOG_SUFFIX="$( date +"%Y%m%d%H%M%S" )"
+  LK8S_LOG_FILE="${LK8S_OUTPUT_DIR}/${LK8S_CLOUDFORMATION_STACKNAME}-${_LOG_SUFFIX}.log"
   
   return 0
 }
@@ -231,26 +282,47 @@ lk8s_run_cloudformation()
     return 1
   }
 
+  local _ANSWER="no"
+  local _TITLE="lightsail-k8s-installer v${LK8S_VERSION}"
+  local _ANY_KEY=""
+  
+  lk8s_char_repeat "-" $( echo $_TITLE | wc -c ) && echo
+  echo $_TITLE
+  lk8s_char_repeat "-" $( echo $_TITLE | wc -c ) && echo
+  echo "This process will create Kubernetes cluster on Amazon Lightsail"
+  echo "Resources: $LK8S_NUMBER_OF_CP_NODES control plane node, $LK8S_NUMBER_OF_WORKER_NODES worker nodes and 1 load balancer"
+  echo "CloudFormation stack: $LK8S_CLOUDFORMATION_STACKNAME"
+  
+  echo
+  read -p "Press any key to continue: " _ANY_KEY
+  echo "This may take several minutes, please wait..."
+  echo "To view detailed log, run following command on another terminal:"
+  echo "  tail -f $LK8S_LOG_FILE"
+  echo
+
+  lk8s_log "Checking existing stack '${LK8S_CLOUDFORMATION_STACKNAME}'"
   # Do not create when the stack already exists
-  aws cloudformation describe-stacks --stack-name=$LK8S_CLOUDFORMATION_STACKNAME >/dev/null 2>&1 && {
-    echo "Stack already exists. SKIP."
+  aws cloudformation describe-stacks --stack-name=$LK8S_CLOUDFORMATION_STACKNAME >>$LK8S_LOG_FILE 2>&1 && {
+    lk8s_log "Stack already exists. Skip."
     return 0
   }
+  
+  lk8s_log "Stack '${LK8S_CLOUDFORMATION_STACKNAME}' is not exists, good!"
 
   ( lk8s_cf_template_header && \
   lk8s_cf_template_control_plane_nodes && \
   lk8s_cf_template_worker_nodes && \
   lk8s_cf_template_load_balancer_worker_nodes ) | \
   aws cloudformation create-stack \
-    --stack-name="${LK8S_CLOUDFORMATION_STACKNAME_PREFIX}-$1" \
-    --template-body file:///dev/stdin
+    --stack-name="$LK8S_CLOUDFORMATION_STACKNAME" \
+    --template-body file:///dev/stdin >> $LK8S_LOG_FILE 2>&1
 
   local STACK_STATUS=""
   local _WAIT_COUNTER=1
   
   while [ "$STACK_STATUS" != "CREATE_COMPLETE" ]
   do
-    echo -ne "\rWaiting stack $LK8S_CLOUDFORMATION_STACKNAME to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )\033[K"
+    lk8s_log_waiting "Waiting stack '$LK8S_CLOUDFORMATION_STACKNAME' to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )"
     STACK_STATUS="$( aws cloudformation describe-stacks \
                     --stack-name="$LK8S_CLOUDFORMATION_STACKNAME" | \
                     jq -r '.Stacks[0].StackStatus' )"
@@ -260,14 +332,15 @@ lk8s_run_cloudformation()
     sleep 2
   done
   
-  echo -e "\rStack '$LK8S_CLOUDFORMATION_STACKNAME' is ready.\033[K"
-    
+  echo
+  lk8s_log "Stack '$LK8S_CLOUDFORMATION_STACKNAME' is ready"
+  
   return $?
 }
 
 lk8s_run_post_command_control_plance_nodes()
 {
-  echo "Running post commands on control plane nodes..."
+  lk8s_log "Running post installation commands on control plane nodes"
   local _ETC_HOSTS="$( lk8s_gen_node_etc_hosts )"
   
   for i in $( seq 1 $LK8S_NUMBER_OF_CP_NODES )
@@ -275,12 +348,13 @@ lk8s_run_post_command_control_plance_nodes()
     local _NODE_NAME=$LK8S_CONTROL_PLANE_NODE_PREFIX-$i
     lk8s_wait_for_node_to_be_ready $_NODE_NAME
     
-    echo "Applying firewall rules on node ${_NODE_NAME}..."
+    lk8s_log "Applying firewall rules on node '${_NODE_NAME}'"
     lk8s_apply_firewall_rules_control_plane_nodes $_NODE_NAME > /dev/null
     
     local _NODE_IP="$( aws lightsail get-instance --instance-name=$_NODE_NAME | jq -r .instance.publicIpAddress )"
-    echo "Installing Kubernetes control plane on node ${_NODE_NAME}..."
-    cat <<EOF | lk8s_ssh_to_node $_NODE_IP sudo -u ec2-user bash
+    lk8s_log "Installing Kubernetes control plane on node ${_NODE_NAME}"
+    
+    cat <<EOF | lk8s_ssh_to_node $_NODE_IP sudo -u ec2-user bash >> $LK8S_LOG_FILE
 [ "\$( hostname )" != "$_NODE_NAME" ] && {
   sudo hostnamectl set-hostname $_NODE_NAME
 }
@@ -303,7 +377,8 @@ grep $_NODE_NAME /etc/hosts >/dev/null || (
 CP_NODE_STATUS="\$( kubectl get nodes --no-headers | awk '{print \$2}' )"
 
 [ "\$CP_NODE_STATUS" = "NotReady" ] && {
-  kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+  curl -s -L https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml | \
+  sed 's#10.244.0.0/16#$LK8S_POD_NETWORK_CIDR#' | kubectl apply -f -
 }
 EOF
   done
@@ -313,7 +388,7 @@ EOF
 
 lk8s_run_post_command_worker_node()
 {
-  echo "Running post commands on worker nodes..."
+  lk8s_log "Running post installation commands on worker nodes"
   local _ETC_HOSTS="$( lk8s_gen_node_etc_hosts )"
   
   for i in $( seq 1 $LK8S_NUMBER_OF_WORKER_NODES )
@@ -321,14 +396,14 @@ lk8s_run_post_command_worker_node()
     local _NODE_NAME=$LK8S_WORKER_NODE_PREFIX-$i
     lk8s_wait_for_node_to_be_ready $_NODE_NAME
     
-    echo "Applying firewall rules on node ${_NODE_NAME}..."
-    lk8s_apply_firewall_rules_worker_node $_NODE_NAME > /dev/null
+    lk8s_log "Applying firewall rules on node '${_NODE_NAME}'"
+    lk8s_apply_firewall_rules_worker_node $_NODE_NAME >> $LK8S_LOG_FILE
     
     local _NODE_IP="$( aws lightsail get-instance --instance-name=$_NODE_NAME | jq -r .instance.publicIpAddress )"
     local _JOIN_CMD=$( lk8s_gen_join_command )
     
-    echo "Joining node $_NODE_NAME to control plane..."
-    cat <<EOF | lk8s_ssh_to_node $_NODE_IP sudo -u ec2-user bash
+    lk8s_log "Joining worker node '$_NODE_NAME' to control plane"
+    cat <<EOF | lk8s_ssh_to_node $_NODE_IP sudo -u ec2-user bash >> $LK8S_LOG_FILE
 [ "\$( hostname )" != "$_NODE_NAME" ] && {
   sudo hostnamectl set-hostname $_NODE_NAME
 }
@@ -426,9 +501,39 @@ lk8s_attach_load_balancer_to_worker_node()
     _INSTANCE_NAMES="$_INSTANCE_NAMES $LK8S_WORKER_NODE_PREFIX-$i"
   done
 
+  lk8s_log "Attaching worker nodes to Lightsail Load Balancer"
   aws lightsail attach-instances-to-load-balancer \
     --load-balancer-name $LK8S_WORKER_LOAD_BALANCER_PREFIX \
-    --instance-names $_INSTANCE_NAMES
+    --instance-names $_INSTANCE_NAMES >> $LK8S_LOG_FILE
+    
+  return 0
+}
+
+lk8s_print_installation_info()
+{
+  local _NODE_NAME=$LK8S_CONTROL_PLANE_NODE_PREFIX-1
+  local _CONTROL_PLANE_IP=$( aws lightsail get-instance --instance-name=$_NODE_NAME | jq -r .instance.publicIpAddress )
+  local _LB_URL=$( aws lightsail get-load-balancer --load-balancer-name $LK8S_WORKER_LOAD_BALANCER_PREFIX | jq -r '.loadBalancer.dnsName' )
+  local _KUBERNETES_INFO="$( lk8s_ssh_to_node $_CONTROL_PLANE_IP kubectl get nodes,services,deployments,pods )"
+  local _INFO=$( cat <<EOF
+Installation COMPLETED.
+
+Your Kubernetes installation info:
+$_KUBERNETES_INFO
+
+Accessing Control Plane via SSH:
+  ssh ec2-user@$_CONTROL_PLANE_IP
+  
+Your app are available via load balancer:
+  http://$_LB_URL
+
+You can view detailed installation log at:
+  $LK8S_LOG_FILE
+
+EOF
+)
+
+  lk8s_log "$_INFO"
 }
 
 lk8s_gen_join_command()
@@ -579,11 +684,13 @@ lk8s_deploy_sample_app()
   local _NODE_NAME=$LK8S_CONTROL_PLANE_NODE_PREFIX-1
   local _NODE_IP="$( aws lightsail get-instance --instance-name=$_NODE_NAME | jq -r .instance.publicIpAddress )"
   
-  echo "Deploying sample apps..."
-  lk8s_wait_for_kubelet_worker_to_be_ready $_NODE_IP 
-  lk8s_gen_sample_configuration_deployment | lk8s_ssh_to_node $_NODE_IP kubectl apply -f -
-  lk8s_wait_for_sample_pods_to_be_ready $_NODE_IP 
-  lk8s_add_node_labels_to_sample_pods $_NODE_IP
+  lk8s_log "Deploying sample http-echo app"
+  lk8s_wait_for_kubelet_worker_to_be_ready $_NODE_IP && (
+    lk8s_gen_sample_configuration_deployment | \
+    lk8s_ssh_to_node $_NODE_IP kubectl apply -f - >> $LK8S_LOG_FILE)
+  lk8s_wait_for_sample_pods_to_be_ready $_NODE_IP && (
+    lk8s_add_node_labels_to_sample_pods $_NODE_IP >> $LK8S_LOG_FILE) && \
+  lk8s_log "Sample app has been deployed."
   
   return 0
 }
@@ -595,22 +702,28 @@ lk8s_wait_for_sample_pods_to_be_ready()
   local _NUMBER_OF_REPLICAS=$(( $LK8S_NUMBER_OF_WORKER_NODES * 2 ))
   local _WAIT_COUNTER=1
   
+  lk8s_log_waiting "Waiting sample pods ($_NUMBER_OF_PODS_READY/${_NUMBER_OF_REPLICAS}) to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )"
+  
   while [ $_NUMBER_OF_PODS_READY -lt $_NUMBER_OF_REPLICAS ]
   do
-    echo -ne "\rWaiting sample pods ($_NUMBER_OF_PODS_READY/${_NUMBER_OF_REPLICAS}) to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )\033[K"
     _NUMBER_OF_PODS_READY=$( cat <<EOF | lk8s_ssh_to_node $_CONTROL_PLANE_IP 
 kubectl get pods --show-kind \
   -l 'action=auto-label-node' -l '!node' --no-headers -o wide \
   --field-selector=status.phase=Running 2>/dev/null | grep -v 'Terminating' | wc -l
 EOF
 )
+
+    lk8s_log_waiting "Waiting sample pods ($_NUMBER_OF_PODS_READY/${_NUMBER_OF_REPLICAS}) to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )"
+    
     [ $_WAIT_COUNTER -ge 3 ] && _WAIT_COUNTER=0
     _WAIT_COUNTER=$(( $_WAIT_COUNTER + 1 ))
     
     sleep 1
   done
 
-  echo -e "\rAll sample pods is ready.\033[K"
+  echo
+  lk8s_log "All sample pods is ready"
+  
   return 0
 }
 
@@ -620,20 +733,25 @@ lk8s_wait_for_kubelet_worker_to_be_ready()
   local _NUMBER_OF_KUBELET_WORKER_READY=0
   local _WAIT_COUNTER=1
   
+  lk8s_log_waiting "Waiting kubelet on worker ($_NUMBER_OF_KUBELET_WORKER_READY/${LK8S_NUMBER_OF_WORKER_NODES}) to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )"
+  
   while [ $_NUMBER_OF_KUBELET_WORKER_READY -lt $LK8S_NUMBER_OF_WORKER_NODES ]
   do
-    echo -ne "\rWaiting kubelet ($_NUMBER_OF_KUBELET_WORKER_READY/${LK8S_NUMBER_OF_WORKER_NODES}) on worker to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )\033[K"
     _NUMBER_OF_KUBELET_WORKER_READY=$( cat <<EOF | lk8s_ssh_to_node $_CONTROL_PLANE_IP 
 kubectl get nodes --selector='!node-role.kubernetes.io/control-plane' --no-headers | grep -v 'NotReady' | wc -l
 EOF
 )
+
+    lk8s_log_waiting "Waiting kubelet on worker ($_NUMBER_OF_KUBELET_WORKER_READY/${LK8S_NUMBER_OF_WORKER_NODES}) to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )"
+    
     [ $_WAIT_COUNTER -ge 3 ] && _WAIT_COUNTER=0
     _WAIT_COUNTER=$(( $_WAIT_COUNTER + 1 ))
     
     sleep 1
   done
 
-  echo -e "\rAll kubelet for worker node is ready.\033[K"
+  echo
+  lk8s_log "All kubelet on worker nodes is ready"
   return 0
 }
 
@@ -650,21 +768,122 @@ lk8s_wait_for_node_to_be_ready()
     # get the IP inside the loop
     _NODE_IP="$( aws lightsail get-instance --instance-name=$_NODE_NAME | jq -r .instance.publicIpAddress )"
     _NODE_STATUS="$( lk8s_ssh_to_node $_NODE_IP whoami 2>/dev/null )"
-    echo -ne "\rWaiting node $_NODE_NAME to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )\033[K"
+    lk8s_log_waiting "Waiting node '$_NODE_NAME' to be ready$( lk8s_char_repeat '.' $_WAIT_COUNTER )"
     
     [ $_WAIT_COUNTER -ge 3 ] && _WAIT_COUNTER=0
     _WAIT_COUNTER=$(( $_WAIT_COUNTER + 1 ))
     
     sleep 2
   done
+
+  echo
+  lk8s_log "Node '$_NODE_NAME' is ready"
   
-  echo -e "\rNode '$_NODE_NAME' is ready.\033[K"
   return 0
 }
 
-lk8s_init && \
-lk8s_run_cloudformation $1 && \
-lk8s_run_post_command_control_plance_nodes && \
-lk8s_run_post_command_worker_node && \
-lk8s_deploy_sample_app &&
-lk8s_attach_load_balancer_to_worker_node
+lk8s_destroy_installation()
+{
+  local _CMD_TO_RUN="aws cloudformation delete-stack --stack-name=$LK8S_CLOUDFORMATION_STACKNAME"
+  
+  [ "$LK8S_DRY_RUN" = "yes" ] && {
+    echo "[DRY RUN] $_CMD_TO_RUN"
+    return 0
+  }
+  
+  local _ANSWER="no"
+  
+  echo "This action will destroy CloudFormation stack '$LK8S_CLOUDFORMATION_STACKNAME'."
+  read -p "Type 'yes' to continue: " _ANSWER
+  
+  [ "$_ANSWER" != "yes" ] && {
+    echo "Aborted."
+    return 0
+  }
+  
+  $_CMD_TO_RUN >> $LK8S_LOG_FILE 2>&1
+  local _WAIT_COUNTER=1
+  
+  echo
+  
+  while :
+  do
+    lk8s_log_waiting "Destroying CloudFormation stack '$LK8S_CLOUDFORMATION_STACKNAME'$( lk8s_char_repeat '.' $_WAIT_COUNTER )"
+    aws cloudformation describe-stacks \
+      --stack-name="$LK8S_CLOUDFORMATION_STACKNAME" 2>>$LK8S_LOG_FILE >>$LK8S_LOG_FILE || break
+    sleep 2
+    
+    [ $_WAIT_COUNTER -ge 3 ] && _WAIT_COUNTER=0
+    _WAIT_COUNTER=$(( $_WAIT_COUNTER + 1 ))
+  done
+  
+  echo
+  lk8s_log "Installation '$LK8S_INSTALLATION_ID' has been destroyed."
+}
+
+# Default action
+LK8S_ACTION="install"
+
+# Parse the arguments
+while getopts c:d:hi:rw:v LK8S_OPT;
+do
+    case $LK8S_OPT in
+        c)
+          LK8S_POD_NETWORK_CIDR="$OPTARG"
+        ;;
+
+        d)
+            LK8S_INSTALLATION_ID="$OPTARG"
+            LK8S_ACTION="destroy"
+        ;;
+
+        h)
+            lk8s_help
+            exit 0
+        ;;
+
+        i)
+            LK8S_INSTALLATION_ID="$OPTARG"
+        ;;
+
+        r)
+            LK8S_DRY_RUN="yes"
+        ;;
+
+        w)
+            LK8S_NUMBER_OF_WORKER_NODES="$OPTARG"
+        ;;
+
+        v)
+            echo "lightsail-k8s-installer version $LK8S_VERSION"
+            exit 0
+        ;;
+
+        \?)
+            echo "Unrecognised option, use -h to see help." >&2
+            exit 1
+        ;;
+    esac
+done
+
+case $LK8S_ACTION in
+  install)
+    lk8s_init && \
+    lk8s_run_cloudformation && \
+    lk8s_run_post_command_control_plance_nodes && \
+    lk8s_run_post_command_worker_node && \
+    lk8s_deploy_sample_app && \
+    lk8s_attach_load_balancer_to_worker_node && \
+    lk8s_print_installation_info
+  ;;
+  
+  destroy)
+    lk8s_init && \
+    lk8s_destroy_installation
+  ;;
+  
+  *)
+    echo "Unrecognised action." >&2
+    exit 1
+  ;;
+esac
